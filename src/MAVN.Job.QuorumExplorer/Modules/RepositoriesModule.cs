a063c8e1f5b8be170d27b91a8ed3d5082eb123a2
anna lizunova <a.lizunova@itransition.com>
2020-05-14 22:01:10 +0300

    Configure registration QeContext

--- src/MAVN.Job.QuorumExplorer/Modules/RepositoriesModule.cs
+++ src/MAVN.Job.QuorumExplorer/Modules/RepositoriesModule.cs
@@ -1,4 +1,6 @@
-﻿using Autofac;
+﻿using System;
+using System.Data.Common;
+using Autofac;
 using JetBrains.Annotations;
 using MAVN.Common.MsSql;
 using Lykke.Job.QuorumExplorer.Settings;
@@ -24,23 +26,26 @@ namespace Lykke.Job.QuorumExplorer.Modules
             ContainerBuilder builder)
         {
             builder
-                .RegisterMsSql(() => new QeContext(_dbSettings.DataConnString, false, _dbSettings.CommandTimeoutSeconds));
+                .RegisterMsSql(
+                    _dbSettings.DataConnString,
+                    connString => new QeContext(connString, false, _dbSettings.CommandTimeoutSeconds),
+                    dbConn => new QeContext(dbConn));
 
             builder
                 .RegisterType<EventRepository>()
                 .As<IEventRepository>()
                 .SingleInstance();
-            
+
             builder
                 .RegisterType<FunctionCallRepository>()
                 .As<IFunctionCallRepository>()
                 .SingleInstance();
-            
+
             builder
                 .RegisterType<TransactionRepository>()
                 .As<ITransactionRepository>()
                 .SingleInstance();
-            
+
             builder
                 .RegisterType<ABIRepository>()
                 .As<IABIRepository>()
